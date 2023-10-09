0afd1c4309562b30e631df93b9264acff61d7c87
brizyriot <brizyriot@yandex.ru>
2023-10-09 14:58:26 +0300

    SQL - исправлен на INNER JOIN test.py - исправлено название функци post_track_order() на get_track_order()  и post_data_order_by_track на get_data_order_by_track

--- configuration.py
+++
@@ -1,3 +0,0 @@
-URL = 'https://6baebacb-f1b3-4837-a055-50182e8b6b51.serverhub.praktikum-services.ru'
-CREATE = '/api/v1/orders'
-TRACK = '/api/v1/orders/track?'
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import orders
 
 
 def test_get_order_by_track():
-    track = orders.post_track_order()
-    data_orders = orders.post_data_order_by_track(track)
+    track = orders.get_track_order()
+    data_orders = orders.get_data_order_by_track(track)
 
     assert data_orders.status_code == 200
